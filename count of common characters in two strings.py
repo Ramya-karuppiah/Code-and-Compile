8faf4fc332859b080203aae36ddc56a41656f31b
Ramya-karuppiah <skmes2012@gmail.com>
2025-06-19 14:57:32 +0530

    Update count of common characters in two strings.py

--- count of common characters in two strings.py
+++ count of common characters in two strings.py
@@ -10,11 +10,10 @@ s1=list(s1)
 s2=list(s2)
 r=0
 for i in range(len(s1)):
-    c=0
+    
     for j in range(len(s2)):
         if s1[i]==s2[j]:
             s2[j]='*'
-            c+=1
-    if c>0:
-        r+=1
+            r+=1
+    
 print(r)
